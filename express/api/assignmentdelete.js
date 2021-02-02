1db83a29373399e4faa041e46980fdacf86dec3e
Two <njsolomonoh@gmail.com>
2021-02-02 18:59:37 -0500

    Add files via upload

--- express/api/assignmentdelete.js
+++ express/api/assignmentdelete.js
@@ -1,15 +1,25 @@
 const validAPI = require("../../web.js").validAPI;
 
 module.exports.load = async function(app, db, settings, manager) {
-  app.post("/api/:id/assignment/delete", async (req, res) => {
+  app.delete("/api/:id/assignment/delete", async (req, res) => {
     let guild = await validAPI(req, res);
     if (guild) {
+      let currentassignment = await db.get(`${req.params.id}-assignment`);
+      if (currentassignment) {
         await db.delete(`${req.params.id}-assignment`);
         res.send(
           {
-            error: "none"
+            error: "none",
+            assignment: currentassignment
           }
         );
+      } else {
+        res.send(
+          {
+            error: "there are no assignments to be deleted"
+          }
+        )
+      }
     };
   });
 }
